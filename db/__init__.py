83130b4bdb750adb2e6e3a5e82ed100759eae00f
Mirriam <mirrmaina@gmail.com>
2018-08-21 16:03:33 +0300

    [Chore: update .gitignore]

--- db/__init__.py
+++ db/__init__.py
@@ -1,15 +1,2 @@
-def mirriam():
-    Mirriam = 'yellow'
-    return Mirriam
 
-print(mirriam())
-
-def color():
-    blueband = mirriam()
-    return blueband
-
-print(color())
-
-blueband = color()
-print(blueband)
 
